78ec0b936028bb99c520ac4e1031735c315679f7
Stephen Montague <me@montaguestephen.com>
2021-12-29 10:27:44 -0500

    Using ternary expressions and in-line styling to dynamically style a component.

--- src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -4,26 +4,41 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+	const [enteredValue, setEnteredValue] = useState('');
+	const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+	const goalInputChangeHandler = event => {
+		setEnteredValue(event.target.value);
+	};
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+	const formSubmitHandler = event => {
+		event.preventDefault();
+		if (enteredValue.trim().length === 0) {
+			setIsValid(!isValid);
+			return;
+		}
+		props.onAddGoal(enteredValue);
+	};
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div className='form-control'>
+				<label
+					style={{ color: !isValid ? 'red' : 'black' }}>
+					Course Goal
+				</label>
+				<input
+					style={{
+						borderColor: !isValid ? 'red' : 'black',
+						background: !isValid ? 'salmon' : 'transparent',
+					}}
+					type='text'
+					onChange={goalInputChangeHandler}
+				/>
+			</div>
+			<Button type='submit'>Add Goal</Button>
+		</form>
+	);
 };
 
 export default CourseInput;
