77a36071ef4a25c3e2611a20b3c8b0212b87fbf2
Stephen Montague <me@montaguestephen.com>
2021-12-29 11:15:14 -0500

    Switched from using inline styling to dynamically changing the class.

--- src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -8,13 +8,16 @@ const CourseInput = props => {
 	const [isValid, setIsValid] = useState(true);
 
 	const goalInputChangeHandler = event => {
+		if (event.target.value.trim().length > 0) {
+			setIsValid(true);
+		}
 		setEnteredValue(event.target.value);
 	};
 
 	const formSubmitHandler = event => {
 		event.preventDefault();
 		if (enteredValue.trim().length === 0) {
-			setIsValid(!isValid);
+			setIsValid(false);
 			return;
 		}
 		props.onAddGoal(enteredValue);
@@ -22,16 +25,12 @@ const CourseInput = props => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className='form-control'>
-				<label
-					style={{ color: !isValid ? 'red' : 'black' }}>
-					Course Goal
-				</label>
+			<div
+				className={`form-control ${
+					!isValid ? 'invalid' : ''
+				}`}>
+				<label>Course Goal</label>
 				<input
-					style={{
-						borderColor: !isValid ? 'red' : 'black',
-						background: !isValid ? 'salmon' : 'transparent',
-					}}
 					type='text'
 					onChange={goalInputChangeHandler}
 				/>
